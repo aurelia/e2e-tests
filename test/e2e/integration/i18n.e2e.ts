ef3aa02505d01b46ba5013e8a2b954d92960ee3b
Sayan751 <sayan751@gmail.com>
2019-07-06 21:10:32 +0200

    chore(i18n): refactor tests post review

--- test/e2e/integration/i18n.e2e.ts
+++ test/e2e/integration/i18n.e2e.ts
@@ -6,139 +6,181 @@ describe('I18N', () => {
     cy.visit('/');
   });
 
-  //#region translation via HTML
-  it('should work for simple html', () => {
-    cy.get(`#i18n-simple[title="${en.simple.attr}"]`).contains(en.simple.text);
+  const dispatchedOn = new Date(2020, 1, 10, 5, 15).toString(), deliveredOn = new Date(2021, 1, 10, 5, 15).toString();
+
+  const tests = [
+    {
+      name: 'should work for simple html', ignoreForDe: true,
+      suts: [{ selector: `#i18n-simple[title="${en.simple.attr}"]`, expected: en.simple.text, expectedDe: undefined }]
+    },
+    {
+      name: 'should work for simple html', ignoreForEn: true,
+      suts: [{ selector: `#i18n-simple[title="${de.simple.attr}"]`, expected: undefined, expectedDe: de.simple.text }]
+    },
+    {
+      name: 'should work for context specific translation',
+      suts: [
+        { selector: `#i18n-ctx`, expected: en.status, expectedDe: de.status },
+        {
+          selector: `#i18n-ctx-dispatched`,
+          expected: en.status_dispatched.replace("{{date}}", dispatchedOn),
+          expectedDe: de.status_dispatched.replace("{{date}}", dispatchedOn),
+        },
+        {
+          selector: `#i18n-ctx-delivered`,
+          expected: en.status_delivered.replace("{{date}}", deliveredOn),
+          expectedDe: de.status_delivered.replace("{{date}}", deliveredOn),
+        },
+      ]
+    },
+    {
+      name: 'should work for pluralization',
+      suts: [
+        {
+          selector: `#i18n-items-0`,
+          expected: en.itemWithCount_plural.replace("{{count}}", "0"),
+          expectedDe: de.itemWithCount_plural.replace("{{count}}", "0")
+        },
+        {
+          selector: `#i18n-items-1`,
+          expected: en.itemWithCount.replace("{{count}}", "1"),
+          expectedDe: de.itemWithCount.replace("{{count}}", "1"),
+        },
+        {
+          selector: `#i18n-items-10`,
+          expected: en.itemWithCount_plural.replace("{{count}}", "10"),
+          expectedDe: de.itemWithCount_plural.replace("{{count}}", "10"),
+        },
+      ]
+    },
+    {
+      name: 'should work for interval processing and nesting',
+      suts: [
+        { selector: `#i18n-interval-0`, expected: "0 items", expectedDe: "0 Artikel" },
+        { selector: `#i18n-interval-1`, expected: "1 item", expectedDe: "1 Artikel" },
+        { selector: `#i18n-interval-2`, expected: "2 items", expectedDe: "2 Artikel" },
+        { selector: `#i18n-interval-3`, expected: "3 items", expectedDe: "3 Artikel" },
+        { selector: `#i18n-interval-6`, expected: "6 items", expectedDe: "6 Artikel" },
+        { selector: `#i18n-interval-7`, expected: "a lot of items", expectedDe: "viele Artikel" },
+        { selector: `#i18n-interval-10`, expected: "a lot of items", expectedDe: "viele Artikel" },
+      ]
+    },
+    {
+      name: 'should work with html content', isHtmlContent: true,
+      suts: [{ selector: `#i18n-html`, expected: en.html, expectedDe: de.html },]
+    },
+    {
+      name: 'should work with "t" value converter, but does not update the value on locale change as it is non-signalable',
+      suts: [{ selector: `#i18n-t-vc`, expected: " 10 items ", expectedDe: " 10 items " },]
+    },
+    {
+      name: 'should work with "t" binding behavior',
+      suts: [{ selector: `#i18n-t-bb`, expected: " 100 items ", expectedDe: " 100 Artikel " },]
+    },
+    {
+      name: 'should work with "nf" value converter',
+      suts: [{ selector: `#i18n-nf-vc`, expected: " 123,456,789.12 ", expectedDe: " 123.456.789,12 " },]
+    },
+    {
+      name: 'should work with "nf" binding behavior',
+      suts: [{ selector: `#i18n-nf-bb`, expected: " 123.456.789,12 ", expectedDe: " 123.456.789,12 " },]
+    },
+    {
+      name: 'should work with "nf" value converter for currency',
+      suts: [{ selector: `#i18n-nf-vc-cur`, expected: " 123.456.789,12\u{00a0}€ ", expectedDe: " 123.456.789,12\u{00a0}€ " },]
+    },
+    {
+      name: 'should work with "nf" binding behavior for currency',
+      suts: [{ selector: `#i18n-nf-bb-cur`, expected: " $123,456,789.12 ", expectedDe: " 123.456.789,12\u{00a0}$ " },]
+    },
+    {
+      name: 'should work with "df" value converter',
+      suts: [{ selector: `#i18n-df-vc`, expected: " 2/10/2020 ", expectedDe: " 10.2.2020 " },]
+    },
+    {
+      name: 'should work with "df" binding behavior',
+      suts: [{ selector: `#i18n-df-bb`, expected: " 10.2.2020 ", expectedDe: " 10.2.2020 " },]
+    },
+    {
+      name: 'should work with "rt" value converter',
+      suts: [{ selector: `#i18n-rt-vc`, expected: " 2 hours ago ", expectedDe: " 2 hours ago " },]
+    },
+    {
+      name: 'should work with "rt" binding behavior',
+      suts: [{ selector: `#i18n-rt-bb`, expected: " 2 hours ago ", expectedDe: " vor 2 Stunden " },]
+    },
+  ]
+
+  describe("translates via HTML that", () => {
+    for (const { name, suts, isHtmlContent, ignoreForEn } of tests) {
+      if (!ignoreForEn) {
+        it(name, () => {
+          for (const sut of suts) {
+            assertContent(sut.selector, sut.expected as string, isHtmlContent);
+          }
+        });
+      }
+    }
   });
 
-  it('should work for context specific translation', () => {
-    cy.get(`#i18n-ctx`).contains(en.status);
-    cy.get(`#i18n-ctx-dispatched`).contains(en.status_dispatched.replace("{{date}}", new Date(2020, 1, 10, 5, 15).toString()));
-    cy.get(`#i18n-ctx-delivered`).contains(en.status_delivered.replace("{{date}}", new Date(2021, 1, 10, 5, 15).toString()));
+
+  describe("updates translation on locale change that", () => {
+
+    beforeEach(() => { cy.get("#locale-changer-de").click(); });
+
+    for (const { name, suts, isHtmlContent, ignoreForDe } of tests) {
+      if (!ignoreForDe) {
+        it(name, () => {
+          for (const sut of suts) {
+            assertContent(sut.selector, sut.expectedDe as string, isHtmlContent);
+          }
+        });
+      }
+    }
   });
 
-  it('should work for pluralization', () => {
-    cy.get(`#i18n-items-0`).contains(en.itemWithCount_plural.replace("{{count}}", "0"));
-    cy.get(`#i18n-items-1`).contains(en.itemWithCount.replace("{{count}}", "1"));
-    cy.get(`#i18n-items-10`).contains(en.itemWithCount_plural.replace("{{count}}", "10"));
-  });
-
-  it('should work for interval processing and nesting', () => {
-    cy.get(`#i18n-interval-0`).should("have.text", "0 items");
-    cy.get(`#i18n-interval-1`).should("have.text", "1 item");
-    cy.get(`#i18n-interval-2`).should("have.text", "2 items");
-    cy.get(`#i18n-interval-3`).should("have.text", "3 items");
-    cy.get(`#i18n-interval-6`).should("have.text", "6 items");
-    cy.get(`#i18n-interval-7`).should("have.text", "a lot of items");
-    cy.get(`#i18n-interval-10`).should("have.text", "a lot of items");
-  });
-
-  it('should work with html content', () => {
-    cy.get(`#i18n-html`).should("have.html", en.html);
-  });
-
-  it('should work with prepend and append', () => {
-    cy.get(`#i18n-prepend-append`).should("have.text", "Mr. Blue Sky");
-  });
-
-  it('should work with "t" value converter', () => {
-    cy.get(`#i18n-t-vc`).contains("10 items");
-  });
-  it('should work with "t" binding behavior', () => {
-    cy.get(`#i18n-t-bb`).contains("100 items");
-  });
-
-  it('should work with "nf" value converter', () => {
-    cy.get(`#i18n-nf-vc`).contains("123,456,789.12");
-  });
-  it('should work with "nf" binding behavior', () => {
-    cy.get(`#i18n-nf-bb`).contains("123.456.789,12");
-  });
-
-  it('should work with "nf" value converter for currency', () => {
-    cy.get(`#i18n-nf-vc-cur`).should("have.text", " 123.456.789,12\u{00a0}€ ");
-  });
-  it('should work with "nf" binding behavior for currency', () => {
-    cy.get(`#i18n-nf-bb-cur`).contains("$123,456,789.12");
-  });
-
-  it('should work with "df" value converter', () => {
-    cy.get(`#i18n-df-vc`).contains("2/10/2020");
-  });
-  it('should work with "df" binding behavior', () => {
-    cy.get(`#i18n-df-bb`).contains("10.2.2020");
-  });
-
-  it('should work with "rt" value converter', () => {
-    cy.get(`#i18n-rt-vc`).contains("2 hours ago");
-  });
-  it('should work with "rt" binding behavior', () => {
-    cy.get(`#i18n-rt-bb`).contains("2 hours ago");
-  });
-  //#endregion
-
-  //#region translation via code
-  it("facilitates translation via code - simple", () => {
-    cy.get("#i18n-code-simple").contains("simple text")
-  });
-  it("facilitates translation via code - context-based", () => {
-    cy.get("#i18n-code-context").contains(`dispatched on ${new Date(2020, 1, 10, 5, 15)}`)
-  });
-  it("facilitates translation via code - plural", () => {
-    cy.get("#i18n-code-plural").contains("10 items")
-  });
-  it("facilitates translation via code - interval", () => {
-    cy.get("#i18n-code-interval").contains("a lot of items")
-  });
-  it("facilitates translation via code - nf", () => {
-    cy.get("#i18n-code-num").contains("123,456,789")
-  });
-  it("facilitates translation via code - df", () => {
-    cy.get("#i18n-code-date").contains("2/10/2021")
-  });
-  it("facilitates translation via code - relative time", () => {
-    cy.get("#i18n-code-rtime").contains("2 hours ago")
-  });
-  //#endregion
-
-  it("can update translation upon change of locale", () => {
-    cy.get("#locale-changer-de").click();
-
-    cy.get(`#i18n-simple[title="${de.simple.attr}"]`).contains(de.simple.text);
-
-    cy.get(`#i18n-ctx`).contains(de.status);
-    cy.get(`#i18n-ctx-dispatched`).contains(de.status_dispatched.replace("{{date}}", new Date(2020, 1, 10, 5, 15).toString()));
-    cy.get(`#i18n-ctx-delivered`).contains(de.status_delivered.replace("{{date}}", new Date(2021, 1, 10, 5, 15).toString()));
-
-    cy.get(`#i18n-items-0`).contains(de.itemWithCount_plural.replace("{{count}}", "0"));
-    cy.get(`#i18n-items-1`).contains(de.itemWithCount.replace("{{count}}", "1"));
-    cy.get(`#i18n-items-10`).contains(de.itemWithCount_plural.replace("{{count}}", "10"));
-
-    cy.get(`#i18n-interval-0`).should("have.text", "0 Artikel");
-    cy.get(`#i18n-interval-1`).should("have.text", "1 Artikel");
-    cy.get(`#i18n-interval-2`).should("have.text", "2 Artikel");
-    cy.get(`#i18n-interval-3`).should("have.text", "3 Artikel");
-    cy.get(`#i18n-interval-6`).should("have.text", "6 Artikel");
-    cy.get(`#i18n-interval-7`).should("have.text", "viele Artikel");
-    cy.get(`#i18n-interval-10`).should("have.text", "viele Artikel");
-
-    cy.get(`#i18n-html`).should("have.html", de.html);
-
-    cy.get(`#i18n-prepend-append`).should("have.text", "Herr. Blue Himmel");
-
-    cy.get(`#i18n-t-vc`).contains("10 items"); //<-- t VC is not signalable
-    cy.get(`#i18n-t-bb`).contains("100 Artikel");
-
-    cy.get(`#i18n-nf-vc`).contains("123.456.789,12"); //<-- nf is equipped with locale
-    cy.get(`#i18n-nf-bb`).contains("123.456.789,12"); //<-- fixed locale, no change is expected
-
-    cy.get(`#i18n-nf-vc-cur`).should("have.text", " 123.456.789,12\u{00a0}€ "); //<-- fixed locale, no change is expected
-    cy.get(`#i18n-nf-bb-cur`).contains("123.456.789,12\u{00a0}$");
-
-    cy.get(`#i18n-df-vc`).contains("10.2.2020");
-    cy.get(`#i18n-df-bb`).contains("10.2.2020"); //<-- fixed locale, no change is expected
-
-    cy.get(`#i18n-rt-vc`).contains("2 hours ago"); //<-- rt VC is not signalable
-    cy.get(`#i18n-rt-bb`).contains("vor 2 Stunden");
+  describe("facilitates translation via code - ", () => {
+    const tests = [
+      {
+        name: "simple",
+        suts: [{ selector: "#i18n-code-simple", expected: "simple text" }]
+      },
+      {
+        name: "context-based",
+        suts: [{ selector: "#i18n-code-context", expected: `dispatched on ${dispatchedOn}` }]
+      },
+      {
+        name: "plural",
+        suts: [{ selector: "#i18n-code-plural", expected: "10 items" }]
+      },
+      {
+        name: "interval",
+        suts: [{ selector: "#i18n-code-interval", expected: "a lot of items" }]
+      },
+      {
+        name: "nf",
+        suts: [{ selector: "#i18n-code-num", expected: "123,456,789" }]
+      },
+      {
+        name: "df",
+        suts: [{ selector: "#i18n-code-date", expected: "2/10/2021" }]
+      },
+      {
+        name: "relative time",
+        suts: [{ selector: "#i18n-code-rtime", expected: "2 hours ago" }]
+      },
+    ];
+    for (const { name, suts } of tests) {
+      it(name, () => {
+        for (const sut of suts) {
+          assertContent(sut.selector, sut.expected);
+        }
+      });
+    }
   });
 });
+
+function assertContent(selector: string, expected: string, isHtmlContent: boolean | undefined = false) {
+  cy.get(selector).should(isHtmlContent ? "have.html" : "have.text", expected);
+}
+
